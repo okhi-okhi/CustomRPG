bcf3f575cc56a54e87ecd1c7edf0822b5dc36968
okhiokhi <okhiokhi3@gmail.com>
2023-04-29 10:44:20 +0800

    MainScreen frontend finish

--- src/Screen/Screens/MainScreen.cpp
+++ src/Screen/Screens/MainScreen.cpp
@@ -9,11 +9,10 @@
 MainScreen::MainScreen() : Screen(screenTypes::MAIN, "main")
 {
 	addElement(FullPicture("screens/main/background.png", 1));
-	addButton(Button("screens/button_1.png", Vector2(0.5f, 0.2f), 0.2f, "screen.test.button1", 0.03f, textAlign::CENTER, BLACK, 0.0f, test));
-	addButton(ButtonArg("screens/button_1.png", Vector2(0.5f, 0.5f), 0.2f, "screen.test.button2", { {"aa", &playerDistance} }, 0.03f, textAlign::CENTER, BLACK, 0.0f, test));
-	// addElement(Picture("screens/button_1.png", Vector2(0.55f, 0.15f), 2, 0.1f));
-	// addElement(Picture("screens/button_1.png", Vector2(0.45f, 0.25f), 2, 0.1f));
-	addButton(Button("screens/button_1.png", Vector2(0.1f, 0.2f), 0.2f, "screen.test.button1", 0.03f, textAlign::CENTER, BLACK, 0.0f, test));
+	addElement(Picture("screens/main/title.png", Vector2(0.5f, 0.1f), 1, 0.5f));
+	addButton(Button("screens/button_1.png", Vector2(0.5f, 0.3f), 0.2f, "screen.test.button1", 0.03f, textAlign::CENTER, WHITE, 0.0f, test));
+	addButton(Button("screens/button_1.png", Vector2(0.5f, 0.5f), 0.2f, "screen.test.button1", 0.03f, textAlign::CENTER, WHITE, 0.0f, test));
+	addButton(Button("screens/button_1.png", Vector2(0.5f, 0.7f), 0.2f, "screen.test.button1", 0.03f, textAlign::CENTER, WHITE, 0.0f, test));
 }
 
 Screen* MainScreen::clone() const
